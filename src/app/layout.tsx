d33eeb42116a691da8e5bae660b6d513351632fc
ken <kenin.qiu@gmail.com>
2025-11-22 14:20:22 -0500

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./[locale]/globals.css";
 
 export const metadata: Metadata = {
@@ -13,6 +14,20 @@ export default function RootLayout({
 }) {
     return (
         <html lang="en" suppressHydrationWarning>
+            <head>
+                <Script
+                    src="https://www.googletagmanager.com/gtag/js?id=G-WQXLTRPLJK"
+                    strategy="afterInteractive"
+                />
+                <Script id="google-analytics" strategy="afterInteractive">
+                    {`
+                        window.dataLayer = window.dataLayer || [];
+                        function gtag(){dataLayer.push(arguments);}
+                        gtag('js', new Date());
+                        gtag('config', 'G-WQXLTRPLJK');
+                    `}
+                </Script>
+            </head>
             <body suppressHydrationWarning>{children}</body>
         </html>
     );
